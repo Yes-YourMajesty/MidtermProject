5d0b402f93d786888f1417014e9bad796a3a7b68
241106 <241106@std.do-joho.ac.jp>
2025-06-23 11:42:25 +0900

    update

--- src/GameMaster.java
+++ src/GameMaster.java
@@ -1,27 +1,31 @@
 public class GameMaster {
     public static void main(String[] args) {
-        Hero h1 = new Hero("アベル",100,"銅の剣");
+        Hero abel = new Hero("アベル",100,"銅の剣");
         Slime s1 =new Slime("スライムA",30);
 
         System.out.println("冒険が始まる…！");
-        h1.showStatus();
+        abel.showStatus();
         s1.showStatus();
         System.out.println("戦闘開始！");
-        h1.attack(s1);
-        h1.showStatus();
-        s1.attack(h1);
-        h1.showStatus();
-        h1.heal();
-        h1.showStatus();
-        h1.attack(s1);
-        h1.attack(s1);
-        h1.attack(s1);
+        abel.attack(s1);
+        abel.showStatus();
+        s1.attack(abel);
+        abel.showStatus();
+        abel.heal();
+        abel.showStatus();
+        abel.attack(s1);
+        abel.attack(s1);
+        abel.attack(s1);
         System.out.println("--- 戦闘終了 ---");
         System.out.println("最終ステータス:");
-        h1.showStatus();
+        abel.showStatus();
         s1.showStatus();
-        h1.isAlive();
+        abel.isAlive();
         s1.isAlive();
         System.out.println("--- 参照の確認 ---");
+        Character partyMember = abel;
+        partyMember.hp -= 50;
+        System.out.println("partyMember経由でHPを減らした後:");
+        abel.showStatus();
     }
 }
